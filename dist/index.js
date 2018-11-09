75e1dcc4c9c14f7054afc75daa4ba0b9875d1056
Nik Coughlin <nrkn.com@gmail.com>
2018-11-09 15:23:11 +1300

    Adds GrayData

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ var is_little_endian_1 = require("./is-little-endian");
 exports.isLittleEndian = is_little_endian_1.isLittleEndian;
 var predicates_1 = require("./predicates");
 exports.isImageData = predicates_1.isImageData;
+exports.isGrayData = predicates_1.isGrayData;
 var rgba_to_uint32_1 = require("./rgba-to-uint32");
 exports.rgbaToUint32 = rgba_to_uint32_1.rgbaToUint32;
 //# sourceMappingURL=index.js.map
